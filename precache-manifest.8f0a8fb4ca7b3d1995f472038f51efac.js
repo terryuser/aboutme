dd2d5de021b21b135a96abee772a4674981973a2
terryuser <homan03@hotmail.com>
2020-10-15 21:42:04 +0800

    Updates

---
+++ precache-manifest.8f0a8fb4ca7b3d1995f472038f51efac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0264e8f3a2648597f763fdc881112cf",
+    "url": "/aboutme/index.html"
+  },
+  {
+    "revision": "af1f7e88a42bd71df891",
+    "url": "/aboutme/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4d151e1114f40217a8bc",
+    "url": "/aboutme/static/js/2.0201300e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/aboutme/static/js/2.0201300e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af1f7e88a42bd71df891",
+    "url": "/aboutme/static/js/main.ed401dec.chunk.js"
+  },
+  {
+    "revision": "955f3070b93d413f0fd9",
+    "url": "/aboutme/static/js/runtime-main.95483440.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/aboutme/static/media/logo.5d5d9eef.svg"
+  }
+]);
